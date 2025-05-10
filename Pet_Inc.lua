4dbf39c21c9d1597c642a02d87d22bbaa889de6a
Fallenfromheaven <156500509+Fallenfromheaven@users.noreply.github.com>
2025-05-10 08:32:26 +0200

    Update Pet_Inc.lua

--- Pet_Inc.lua
+++ Pet_Inc.lua
@@ -2,12 +2,53 @@
 
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
+local suffixes = {
+	["K"]   = 1e3,
+	["M"]   = 1e6,
+	["B"]   = 1e9,
+	["T"]   = 1e12,
+	["Qd"]  = 1e15,
+	["Qn"]  = 1e18,
+    ["Sx"]  = 1e21,
+    ["Sp"]  = 1e24,
+    ["Oc"]  = 1e27,
+    ["N"]   = 1e30,
+    ["Dc"]  = 1e33,
+    ["Ud"]  = 1e36,
+    ["Dd"]  = 1e39,
+    ["Td"]  = 1e42,
+    ["Qdd"] = 1e45
+}
+
+---------------- custom funcs
+
+function ReverseFormattedNumber(input)
+	input = input:gsub("%s+", "")
+
+	local numPart, suffix = input:match("([%d%.]+)([A-Za-z]+)")
+	if numPart and suffixes[suffix] then
+		return tonumber(numPart) * suffixes[suffix]
+	end
+
+	local clean = input:gsub(",", "")
+	local num = tonumber(clean)
+	if num then
+		return num
+	else
+		error("Invalid input format: " .. tostring(input))
+	end
+end
+
+---------------- Function dependant Vars
+
+-- empty for now
+
 ---------------- UI 
 
 local Window = Rayfield:CreateWindow({
-   Name = "Scriptloader",
+   Name = "Pet Incremental",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "Der Ultimative Scriptloader :)",
+   LoadingTitle = "The ultimate Pet Incremental Script :)",
    LoadingSubtitle = "by Fallen_from_heaven",
    Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
@@ -16,27 +57,41 @@ local Window = Rayfield:CreateWindow({
 
    ConfigurationSaving = {
       Enabled = true,
-      FolderName = "Scriptloader",
+      FolderName = "PetInc",
       FileName = "loader-data"
    }
 })
 
 local PetInc = Window:CreateTab("Pet Incremental", 4483362458)
 
----------------- UI BUTTONS + FUNCTIONS
+---------------- UI BUTTONS
+local Toggle_0 = PetInc:CreateToggle({
+    Name = "Auto-Open-Eggs (requieres close proximity untill rank3)",
+    CurrentValue = false,
+    Flag = "toggle_0",
+    Callback = function(Value)
+        while Rayfield.Flags["toggle_0"].CurrentValue do
+            local args = {"OpenEgg"}
+            game:GetService("ReplicatedStorage"):WaitForChild("RE"):FireServer(unpack(args))
+            task.wait(1)
+            end
+        end
+    })
+local Divider_0 = PetInc:CreateDivider()
 
 local Toggle_1 = PetInc:CreateToggle({
-    Name = "Auto-Evolve-all-pets",
+    Name = "Auto-Evolve-all-Pets",
     CurrentValue = false,
     Flag = "toggle_1",
     Callback = function(Value)
         while Rayfield.Flags["toggle_1"].CurrentValue do
-            local args = {"EquipBest"}
+            local args = {"EvolveAll"}
             game:GetService("ReplicatedStorage"):WaitForChild("RE"):FireServer(unpack(args))
-            task.wait(2)
+            task.wait(1)
             end
         end
     })
+local Divider_1 = PetInc:CreateDivider()
 
 local Toggle_2 = PetInc:CreateToggle({
     Name = "Auto-Equip-best-Pets",
@@ -46,12 +101,87 @@ local Toggle_2 = PetInc:CreateToggle({
         while Rayfield.Flags["toggle_2"].CurrentValue do
             local args = {"EquipBest"}
             game:GetService("ReplicatedStorage"):WaitForChild("RE"):FireServer(unpack(args))
+            task.wait(1)
+            end
+        end
+    })
+local Divider_2 = PetInc:CreateDivider()
+
+local Toggle_3 = PetInc:CreateToggle({
+    Name = "Auto-Coin-Upgrades",
+    CurrentValue = false,
+    Flag = "toggle_3",
+    Callback = function(Value)
+        while Rayfield.Flags["toggle_3"].CurrentValue do
+            
+            local path = game:GetService("Players").LocalPlayer.PlayerGui.CoinUpgrades.Bg.List
+            local UpgradeList  = {
+                ["CoinMulti"]  = ReverseFormattedNumber(game:GetService("Players").LocalPlayer.PlayerGui.CoinUpgrades.Bg.List.CoinMulti.Buy.Cost.text),
+                ["EggSpeed"]   = ReverseFormattedNumber(game:GetService("Players").LocalPlayer.PlayerGui.CoinUpgrades.Bg.List.EggSpeed.Buy.Cost.text),
+                ["EggLuck"]    = ReverseFormattedNumber(game:GetService("Players").LocalPlayer.PlayerGui.CoinUpgrades.Bg.List.EggLuck.Buy.Cost.text),
+                ["PetEquip"]   = ReverseFormattedNumber(game:GetService("Players").LocalPlayer.PlayerGui.CoinUpgrades.Bg.List.PetEquip.Buy.Cost.text),
+                ["PetStorage"] = ReverseFormattedNumber(game:GetService("Players").LocalPlayer.PlayerGui.CoinUpgrades.Bg.List.PetStorage.Buy.Cost.text),
+                ["EggClone"]   = ReverseFormattedNumber(game:GetService("Players").LocalPlayer.PlayerGui.CoinUpgrades.Bg.List.EggClone.Buy.Cost.text),
+            }
+            for key, value in pairs(UpgradeList) do    
+                if path[key].Max.Visible == true then
+                    UpgradeList[key] = nil
+                end
+            end
+
+            local lowestKey, lowestValue = nil, nil
+            local rawText = game:GetService("Players").LocalPlayer.PlayerGui.UI.Stats.Coins.TextLabel.Text
+            local numericText = ReverseFormattedNumber(rawText)
+
+            for key, value in pairs(UpgradeList) do
+                if value and (not lowestValue or value < lowestValue) then
+                    lowestKey, lowestValue = key, value
+                end
+            end
+
+            local args = {"CoinUpgrade", lowestKey}
+            if tonumber(numericText) >= lowestValue then
+                game:GetService("ReplicatedStorage"):WaitForChild("RE"):FireServer(unpack(args))
+            end
+            task.wait(0.5)
+
+            end
+        end
+    })
+local Divider_3 = PetInc:CreateDivider()
+
+local Toggle_4 = PetInc:CreateToggle({
+    Name = "Auto-Rank-Up",
+    CurrentValue = false,
+    Flag = "toggle_4",
+    Callback = function(Value)
+        while Rayfield.Flags["toggle_4"].CurrentValue do
+            break
+            end
+        end
+    })
+        
+
+local Toggle_5 = PetInc:CreateToggle({
+    Name = "Auto-Rank-Up",
+    CurrentValue = false,
+    Flag = "toggle_5",
+    Callback = function(Value)
+        while Rayfield.Flags["toggle_5"].CurrentValue do
+            local rankup_cost = ReverseFormattedNumber(game:GetService("Players").LocalPlayer.PlayerGui.RankUp.Bg.List.Buy.Cost.text)
+            local rawText = game:GetService("Players").LocalPlayer.PlayerGui.UI.Stats.Coins.TextLabel.Text
+            local numericText = ReverseFormattedNumber(rawText)
+            local args = {"RankUp"}
+            if numericText >= rankup_cost then
+                game:GetService("ReplicatedStorage"):WaitForChild("RE"):FireServer(unpack(args))
+            end
             task.wait(2)
             end
         end
     })
 
----------------- Settings + lin_init
+
+---------------- Settings + lib_init
 
 local settings_ = Window:CreateTab("Settings", 4483362458)
 
